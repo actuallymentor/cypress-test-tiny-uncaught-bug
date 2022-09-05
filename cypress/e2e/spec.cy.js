72e587c55e260df65019d96b0125b9730739934e
Mentor <mentor@palokaj.co>
2022-09-05 12:11:02 +0200

    ✨ complete issue demp

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000')
+    cy.on( 'window:alert', response => {
+        expect( response ).to.contain( 'Something the alert does not contain' )
+    } )
+    cy.contains( 'Learn React' )
   })
 })
